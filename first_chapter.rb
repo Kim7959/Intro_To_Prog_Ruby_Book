e7cdeaa5959c1de8702d64c9f6f60d70aff980e4
Kim Kvarda <Kimkvarda@yahoo.com>
2022-06-06 20:11:41 +0000

    2nd try

--- first_chapter.rb
+++ first_chapter.rb
@@ -9,4 +9,4 @@ second_hash = { Star_Wars: 1977, The_Empire_Strikes_Back: 1980, Return_of_the_Je
 movie_hash.each {|x,y| puts "#{y}"}
 date_array = [1977, 1980, 1983]
 date_array.each {|x| puts x}
-date_array.each {|x| puts x}
+date_array = [1977, 1980, 1983]
