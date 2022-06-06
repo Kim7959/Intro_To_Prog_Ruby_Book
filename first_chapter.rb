56ada2a7a412a776d280f579b0071f9739c05baa
Kim Kvarda <Kimkvarda@yahoo.com>
2022-06-06 19:21:32 +0000

    First files

---
+++ first_chapter.rb
@@ -0,0 +1,12 @@
+# The Basics Exercises
+movie_hash =  {:Star_Wars => 1977, :The_Empire_Strikes_Back => 1980, :Return_of_the_Jedi => 1983}
+movie_hash.each {|x,y| puts "#{y}"}
+puts movie_hash [:Star_Wars]
+puts movie_hash [:The_Empire_Strikes_Back]
+puts movie_hash [:Return_of_the_Jedi]
+
+second_hash = { Star_Wars: 1977, The_Empire_Strikes_Back: 1980, Return_of_the_Jedi: 1983}
+movie_hash.each {|x,y| puts "#{y}"}
+date_array = [1977, 1980, 1983]
+date_array.each {|x| puts x}
+date_array.each {|x| puts x}
