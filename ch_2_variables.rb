2b6f34c93e1ded2fd1b0564e8ed22eff9ce24d29
Kim Kvarda <Kimkvarda@yahoo.com>
2022-06-07 15:02:48 +0000

    2nd chapter

---
+++ ch_2_variables.rb
@@ -0,0 +1,23 @@
+puts "What is your first name?"
+first_name = gets.chomp
+puts "What is your last name?"
+last_name = gets.chomp
+puts "Your name is " + first_name + " " + last_name
+
+
+puts "How old are you now?"
+age = gets.chomp.to_i
+puts "In 10 years you will be:"
+puts age + 10
+puts "In 20 years you will be:" 
+puts age + 20
+puts "In 30 years you will be:"
+puts age + 30
+puts "In 40 years you will be:"
+puts age + 40
+
+
+
+
+
+
