4a71d163fd517fb2eb4e87f2e79a3c2954390923
Kim Kvarda <Kimkvarda@yahoo.com>
2022-06-09 14:56:48 +0000

    Chapter 3

---
+++ ch_3_methods.rb
@@ -0,0 +1,19 @@
+def greeting(name)
+  "Hello "+ name + ", welcome to Endor!"
+end 
+
+puts greeting ("Boba Fett")
+  
+
+def multiply(number1, number2)
+  number1 * number2
+end
+
+p multiply(5,2)
+
+def scream(words)
+  words = words + "!!!!"
+  puts words
+end
+
+scream("Yippeee")
