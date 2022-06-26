58079ab4df51a9233f9676890b2496e99fe29311
Kim Kvarda <Kimkvarda@yahoo.com>
2022-06-26 15:10:42 +0000

    in progress

---
+++ ch_7_Hashes/ch_7_exercises.rb
@@ -0,0 +1,13 @@
+# exercise 1
+
+family = {  uncles: ["bob", "joe", "steve"],
+            sisters: ["jane", "jill", "beth"],
+            brothers: ["frank","rob","david"],
+            aunts: ["mary","sally","susan"]
+          }
+
+close_family = family.select {|k,v| k == :sisters || k == :brothers}
+p close_family
+
+array = close_family.values.flatten
+p array
