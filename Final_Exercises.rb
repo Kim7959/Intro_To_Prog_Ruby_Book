e3d6554ff31c9c6e81d3fd86773a125d900bfc17
Kim Kvarda <Kimkvarda@yahoo.com>
2022-06-29 15:08:14 +0000

    final

--- Final_Exercises.rb
+++ Final_Exercises.rb
@@ -47,7 +47,54 @@ h = {a:1, b:2, c:3, d:4}
  h.delete_if { |k, v| v < 3.5 }
  puts h
  
+# Exercise 11
 
+contact_data = [["joe@email.com", "123 Main st.", "555-123-4567"],
+            ["sally@email.com", "404 Not Found Dr.", "123-234-3454"]]
+
+contacts = {"Joe Smith" => {}, "Sally Johnson" => {}}
+
+
+contacts["Joe Smith"][:email] = contact_data[0][0]
+contacts["Joe Smith"][:address] = contact_data[0][1]
+contacts["Joe Smith"][:phone] = contact_data[0][2]
+contacts["Sally Johnson"][:email] = contact_data[1][0]
+contacts["Sally Johnson"][:address] = contact_data[1][1]
+contacts["Sally Johnson"][:phone] = contact_data[1][2]
+puts contacts
+
+puts  contacts["Joe Smith"][:email]
+puts  contacts["Sally Johnson"][:phone]
+
+#Exercise 13
+
+arr = ['snow', 'winter', 'ice', 'slippery', 'salted roads', 'white trees']
+
+puts arr.delete_if {|word| word.start_with?("s")}
+
+arr = ['snow', 'winter', 'ice', 'slippery', 'salted roads', 'white trees']
+
+puts arr.delete_if {|word| word.start_with?("s", "w")}
+
+#Exercise 14
+
+a = ['white snow', 'winter wonderland', 'melting ice',
+     'slippery sidewalk', 'salted roads', 'white trees']
+     
+p a = a.map {|x| x.split} 
+p a.flatten
+
+#Challenge
+
+contact_data = ["joe@email.com", "123 Main st.", "555-123-4567"]
+contacts = {"Joe Smith" => {}}
+fields = [:email, :address, :phone]
+
+contacts.each do |name, hash|
+  fields.each do |field|
+    hash[field] = contact_data.shift
+  end
+end
 
 
 
